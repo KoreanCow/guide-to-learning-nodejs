84e738364e939820c8e29da56d09031fe8bae851
KoreanCow <tyunnon1257@naver.com>
2019-01-25 15:59:45 +0900

    :)

--- Zerocho_Nodejs/learn-sequelize/learn-sequelize/models/index.js
+++ Zerocho_Nodejs/learn-sequelize/learn-sequelize/models/index.js
@@ -13,8 +13,8 @@ db.Sequelize = Sequelize;
 db.User = require('./user')(sequelize, Sequelize);
 db.Comment = require('./comment')(sequelize, Sequelize);
 
-db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id'});               // 1:N 일 때 
-db.Comment.belongsTo(db.User, {foreignKey: 'commenter', targetKey: 'id'});
+db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });               // 1:N 일 때 
+db.Comment.belongsTo(db.User, { foreignKey: 'commenter', targetKey: 'id' });
 
 //      db.User.hasOne(db.Comment, { foreignKey: 'commenter', sourceKey: 'id'});                 1:1 일 때 hasMany -> hasOne
 //      db.Comment.belongsTo(db.User, { foreingKey: 'commenter', targetKey: 'id'});                 hasOne belongTo 가 바뀌어도 상관없음 -> 1:1이니까
@@ -22,6 +22,32 @@ db.Comment.belongsTo(db.User, {foreignKey: 'commenter', targetKey: 'id'});
 //      db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });       ex) 게시글정보를 담고있는 가상의 Post 모델과 해시태그 정보를 담고있는 가상의 Hashtag모델 
 //      db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag' });       N:M 일 때
 module.exports = db;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 'use strict';        원본
 
