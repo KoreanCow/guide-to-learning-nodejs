a36c9b83b807f44497e3a39382b667a4456211e3
KoreanCow <tyunnon1257@naver.com>
2019-02-01 08:38:51 +0900

    async/await

--- Zerocho_Nodejs/learn-sequelize/learn-sequelize/routes/index.js
+++ Zerocho_Nodejs/learn-sequelize/learn-sequelize/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var User = require('../models').User;
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  User.findAll()
+    .then((user) => {
+      res.render('sequelize', { users });
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    })
+  
+    
 });
-
+//        async/await 문법으로 표현한 라우터 
+// router.get('/', async (req, res, next) => {
+//   try{
+//     const users = await User.findAll();
+//     res.render('sequelize', { users });
+//   }catch (error) {
+//     console.error(error);
+//     next(error);
+//   }
+// });
 module.exports = router;
--- Zerocho_Nodejs/learn-sequelize/learn-sequelize/routes/users.js
+++ Zerocho_Nodejs/learn-sequelize/learn-sequelize/routes/users.js
@@ -1,9 +1,62 @@
 var express = require('express');
+var User = require('../models').User;
+
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//        async/await 형식
+router.get('/', function(req, res, next){
+  try{
+    const users = await User.findAll();
+    res.json(users);
+  }catch(err) {
+    console.error(err);
+    next(err);
+  }
 });
 
+router.post('/', function (req, res, next){
+    try{
+      const result = await User.create({
+        name: req.body.name,
+        age: req.body.age,
+        married: req.body.married,
+      });
+      console.log(result);
+      res.status(201).json(result);
+    }catch(err) {
+      console.error(err);
+      next(err);
+    }
+})
+//      프로미스형식
+// router.get('/', function(req, res, next) {
+//   User.findAll()
+//     .then((user) => {
+//       res.json(users);
+//     })
+//     .catch ( (err) => {
+//       console.log(err);
+//     })
+// });
+
+// router.post('/', function (req, res, next) {
+//   User.create({
+//     name: req.body.name,
+//     age: req.body.age,
+//     married: req.body.married,
+//   })
+//     .then((result) => {
+//       console.log(result);
+//       res.status(201).json(result);
+//     })
+//       .catch((err) => {
+//         console.error(err);
+//         next(err);
+//       });
+// });
+
+
 module.exports = router;
+
+
+
